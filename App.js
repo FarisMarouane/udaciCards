06f22fdefc42d591fe805efd6067fd49a510f187
Marouane Faris <faris.marouane@gmail.com>
2018-04-03 21:49:59 +0200

    refactor: Move utils folder outside of components folder

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-import { setLocalNotification } from './components/utils/helpers';
+import { setLocalNotification } from './utils/helpers';
 import Main from './components/main';
 
 export default class App extends React.Component {
